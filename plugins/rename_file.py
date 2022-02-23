2082c15d5d66409b392e055d11a383cee185ec1e
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-02-23 11:44:10 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -55,7 +55,7 @@ async def set_caption(bot, update):
         await update.reply_text(f"**--Your Caption--:**\n\n{CSTM_FIL_CPTN}", quote=True)
 
 
-@app.on_message(pyrogram.filters.command(["rename"]))
+@Mai_bOTs.on_message(pyrogram.filters.command(["rename"]))
 async def rename_doc(bot, update):
     update_channel = Config.UPDATE_CHANNEL
     if update_channel:
